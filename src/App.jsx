d4c0087a361436c18d563542bd2a1809e786f8aa
juanvacag <>
2022-06-27 21:10:18 +0200

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Container, Grid, Typography } from '@mui/material'
+import Formulario from './components/Formulario'
+
+function App() {
+
+  return (
+  <Container>
+    <header>
+      <Typography align="center" marginY={5} component='h1' variant='h2'>
+        Buscador de Noticias
+      </Typography>
+    </header>
+
+    <Formulario />
+    
+  </Container>
+  )
+}
+
+export default App
